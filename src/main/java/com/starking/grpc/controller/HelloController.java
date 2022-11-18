d6018835e7cbe6a7850b97f2ac3f3b541ab285f5
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-11-18 17:48:04 -0300

    criando metodos com proto

--- src/main/java/com/starking/grpc/controller/HelloController.java
+++ src/main/java/com/starking/grpc/controller/HelloController.java
@@ -1,17 +1,25 @@
-//package com.starking.grpc.controller;
-//
-//import io.grpc.stub.StreamObserver;
-//import net.devh.boot.grpc.server.service.GrpcService;
-//
-//@GrpcService
-//public class HelloController extends HelloServiceGrpc.HelloServiceImplBase {
-//    @Override
-//    public void hello(HelloReq request, StreamObserver<HelloRes> responseObserver) {
-//        var response = HelloRes.newBuilder()
-//                .setMessage(request.getMessage())
-//                .build();
-//
-//        responseObserver.onNext(response);
-//        responseObserver.onCompleted();
-//    }
-//}
+package com.starking.grpc.controller;
+
+import com.starking.grpc.*;
+
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class HelloController extends HelloServiceGrpc.HelloServiceImplBase {
+
+	@Override
+	public void create(ProductRequest request, StreamObserver<ProductResponse> responseObserver) {
+		var response = ProductResponse.newBuilder().setField(null, responseObserver).build();
+
+		responseObserver.onNext(response);
+		responseObserver.onCompleted();
+	}
+	
+	@Override
+	public void findAll(EmptyRequest request, StreamObserver<ProductResponseList> responseObserver) {
+		// TODO Auto-generated method stub
+		super.findAll(request, responseObserver);
+	}
+
+}
